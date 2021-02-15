9a98c4d668b33cb7c9b8cd139b9233824a9177ea
Filipe Heaton ponce cabral pinto <fheaton-@c1r19s10.42lisboa.com>
2021-02-15 18:11:29 +0000

    forgot cast on if

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fheaton- <fheaton-@students.42.fr>         +#+  +:+       +#+        */
+/*   By: fheaton- <fheaton-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 01:17:37 by marvin            #+#    #+#             */
-/*   Updated: 2021/02/12 15:26:23 by fheaton-         ###   ########.fr       */
+/*   Updated: 2021/02/15 18:10:51 by fheaton-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	a = ft_strlen(s);
 	while (a >= 0)
 	{
-		if (s[a] == c)
+		if (s[a] == (unsigned char)c)
 			return ((char*)&s[a]);
 		a--;
 	}
